6e6ece98e4b99e8d6f94a51738da45bee1a765a1
ravenberserk <raven.berserk@gmail.com>
2018-10-01 12:33:18 +0200

    Configuracion de la seguridad de los modulos Gateway y Log.

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApp.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApp.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableOAuth2Client
 @EnableZuulProxy
 public class GatewayServiceApp {
 
