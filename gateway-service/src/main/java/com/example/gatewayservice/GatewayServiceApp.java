da79aebaf6f1c2bb82e6aa5a9befde75b3123916
ravenberserk <raven.berserk@gmail.com>
2018-10-17 14:08:40 +0200

    Se sustituye Zuul por Spring Cloud Gateway.

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApp.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApp.java
@@ -3,11 +3,9 @@ package com.example.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class GatewayServiceApp {
 
     public static void main(String[] args) {
