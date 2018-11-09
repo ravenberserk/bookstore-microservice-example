49c4d563702acea7aae9e8bc9cd94f68554dc861
ravenberserk <raven.berserk@gmail.com>
2018-11-09 10:23:28 +0100

    Se modifican los endPoints para registrar las acciones en el log.

--- bookstore-service/src/main/java/com/example/bookstoreservice/controller/BookController.java
+++ bookstore-service/src/main/java/com/example/bookstoreservice/controller/BookController.java
@@ -1,24 +1,23 @@
 package com.example.bookstoreservice.controller;
 
-import java.util.NoSuchElementException;
-
-import javax.validation.Valid;
-
+import com.example.bookstoreservice.domain.Book;
+import com.example.bookstoreservice.service.BookService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.example.bookstoreservice.domain.Book;
-import com.example.bookstoreservice.service.BookService;
+import javax.validation.Valid;
+import java.util.NoSuchElementException;
+
 
 @RestController
 public class BookController {
+
+    private static Log log = LogFactory.getLog(BookController.class);
 
     @Autowired
     private BookService bookService;
@@ -26,19 +25,22 @@ public class BookController {
     @PostMapping(path = "/")
     @PreAuthorize("#oauth2.hasScope('server')")
     public ResponseEntity<Book> createNewBook(@Valid @RequestBody Book book) {
-	return new ResponseEntity<>(bookService.create(book.getTitle(), book.getSummary()), HttpStatus.CREATED);
+        log.info("Se va a crear un nuevo libro con los datos: " + book);
+        return new ResponseEntity<>(bookService.create(book.getTitle(), book.getSummary()), HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/")
     @PreAuthorize("#oauth2.hasScope('server')")
     public ResponseEntity<Iterable<Book>> findAllBooks() {
-	return new ResponseEntity<>(bookService.findAll(), HttpStatus.OK);
+        log.info("Se recuperan todos los libros.");
+        return new ResponseEntity<>(bookService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{bookId}")
     public ResponseEntity<Book> getBook(@PathVariable("bookId") String bookId) {
-	return new ResponseEntity<>(bookService.findBook(bookId).orElseThrow(NoSuchElementException::new),
-		HttpStatus.OK);
+        log.info("Se busca el libro con Id: " + bookId);
+        return new ResponseEntity<>(bookService.findBook(bookId).orElseThrow(NoSuchElementException::new),
+                HttpStatus.OK);
     }
 
 }
