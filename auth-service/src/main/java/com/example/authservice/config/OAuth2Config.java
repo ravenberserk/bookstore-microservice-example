27fc3248bc69093ace2402792b567f6281b61a37
ravenberserk <raven.berserk@gmail.com>
2019-01-30 13:27:38 +0100

    Modificacion de las variables con las passwords.

--- auth-service/src/main/java/com/example/authservice/config/OAuth2Config.java
+++ auth-service/src/main/java/com/example/authservice/config/OAuth2Config.java
@@ -3,6 +3,7 @@ package com.example.authservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -26,17 +27,20 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private Environment env;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         // @formatter:off
         clients.inMemory()
-                .withClient("book-serv")
-                .secret(passwordEncoder.encode("${BOOKSERV-PASSWORD}"))
+                .withClient("bookserv")
+                .secret(passwordEncoder.encode(env.getProperty("BOOKSERV_PASSWORD")))
                 .authorizedGrantTypes("client_credentials", "refresh_token")
                 .scopes("server")
             .and()
                 .withClient("browser")
-                .secret(passwordEncoder.encode("${BROWSER-PASSWORD}"))
+                .secret(passwordEncoder.encode(env.getProperty("BROWSER_PASSWORD")))
                 .authorizedGrantTypes("authorization_code", "refresh_token")
                 .scopes("ui")
                 .autoApprove(true)
