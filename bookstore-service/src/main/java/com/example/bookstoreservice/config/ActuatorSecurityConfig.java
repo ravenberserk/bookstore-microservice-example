5772bcb207aca8c50b9967d521ba2a5ba47313ca
ravenberserk <raven.berserk@gmail.com>
2019-02-06 10:39:37 +0100

    Se habilita la autenticacion basica para Actuator.

---
+++ bookstore-service/src/main/java/com/example/bookstoreservice/config/ActuatorSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.bookstoreservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(1)
+public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/actuator/**").authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+}
